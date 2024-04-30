c91490953c45dd89b325bec762a659e3116add3a
happyprogfrog <happyprogfrog@gmail.com>
2024-04-30 21:27:19 +0900

    6. 주문 도메인 개발

---
+++ src/main/java/jpabook/jpastudy/service/OrderService.java
@@ -0,0 +1,49 @@
+package jpabook.jpastudy.service;
+
+import jpabook.jpastudy.domain.*;
+import jpabook.jpastudy.domain.item.Item;
+import jpabook.jpastudy.repository.ItemRepository;
+import jpabook.jpastudy.repository.MemberRepository;
+import jpabook.jpastudy.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
+    private final ItemRepository itemRepository;
+
+    // 주문
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        // 배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+        delivery.setStatus(DeliveryStatus.READY);
+
+        // 주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        // 주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // 주문 저장
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    // 주문 취소
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        Order order = orderRepository.findOne(orderId);
+        order.cancel();
+    }
+}
---
+++ src/test/java/jpabook/jpastudy/service/OrderServiceTest.java
@@ -0,0 +1,101 @@
+package jpabook.jpastudy.service;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jpabook.jpastudy.domain.Address;
+import jpabook.jpastudy.domain.Member;
+import jpabook.jpastudy.domain.Order;
+import jpabook.jpastudy.domain.OrderStatus;
+import jpabook.jpastudy.domain.item.Book;
+import jpabook.jpastudy.domain.item.Item;
+import jpabook.jpastudy.exception.NotEnoughStockException;
+import jpabook.jpastudy.repository.OrderRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class OrderServiceTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    public void 상품주문() throws Exception {
+        // given
+        Member member = createMember();
+        Item item = createBook("얍얍얍", 10000, 10);
+        int orderCount = 2;
+
+        // when
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+
+        // then
+        Order gotOrder = orderRepository.findOne(orderId);
+
+        assertEquals("상품 주문 시 상태는 ORDER", OrderStatus.ORDER, gotOrder.getStatus());
+        assertEquals("주문한 상품의 수가 정확해야 한다", 1, gotOrder.getOrderItems().size());
+        assertEquals("주문 가격은 가격 * 수량이다.", 10000 * 2, gotOrder.getTotalPrice());
+        assertEquals("주문 수량만큼 재고가 줄어야 한다.", 8, item.getStockQuantity());
+    }
+
+    @Test(expected = NotEnoughStockException.class)
+    public void 상품주문_재고수량초과() throws Exception {
+        // given
+        Member member = createMember();
+        Item item = createBook("얍얍얍", 10000, 10);
+        int orderCount = 11;
+
+        // when
+        orderService.order(member.getId(), item.getId(), orderCount);
+
+        // then
+        fail("재고 수량 부족 예외가 발생해야 한다.");
+    }
+
+    @Test
+    public void 주문취소() {
+        // given
+        Member member = createMember();
+        Item item = createBook("얍얍얍", 10000, 10);
+        int orderCount = 2;
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+
+        // when
+        orderService.cancelOrder(orderId);
+
+        // then
+        Order gotOrder = orderRepository.findOne(orderId);
+        assertEquals("주문 취소 시 상태는 CANCEL이다.", OrderStatus.CANCEL, gotOrder.getStatus());
+        assertEquals("주문이 취소된 상품은 그만큼 재고가 증가해야 한다.", 10, item.getStockQuantity());
+    }
+
+    private Member createMember() {
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울", "강가", "123-123"));
+        em.persist(member);
+        return member;
+    }
+
+    private Book createBook(String name, int price, int stockQuantity) {
+        Book book = new Book();
+        book.setName(name);
+        book.setStockQuantity(stockQuantity);
+        book.setPrice(price);
+        em.persist(book);
+        return book;
+    }
+}
